4742b93e705a5d52af269f75bdf042bccd85b027
Yuuki-Imagawa0827 <yuuki.imagawa0827@gmail.com>
2023-10-19 08:17:44 +0900

    インデント修正

--- controller/userController.ts
+++ controller/userController.ts
@@ -6,20 +6,20 @@ import userService from '../service/userService';
 
 
 // 新規会員登録
-const signupUser = async (req:any, res: Response):Promise<void> => {
-    try {
-        const userData=req.body;
-        if (userData==null) {
-            res.status(400).json({ error: '無効なリクエストデータです。' });
-            return 
-        }
-        const newUser: User = await userService.signupUser(userData);
-        res.status(200).json(newUser);
-    } catch (error) {
-        res.status(500).json({ error: '会員登録に失敗しました。' });
+const signupUser = async (req: any, res: Response): Promise<void> => {
+  try {
+    const userData = req.body;
+    if (userData == null) {
+      res.status(400).json({ error: '無効なリクエストデータです。' });
+      return
     }
+    const newUser: User = await userService.signupUser(userData);
+    res.status(200).json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: '会員登録に失敗しました。' });
+  }
 };
 
 export default {
-    signupUser,
+  signupUser,
 }
--- service/userService.ts
+++ service/userService.ts
@@ -4,30 +4,30 @@ import type User from '../interface/User'
 import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient({
-    log: ['query', 'info', 'warn', 'error'],
-  });
+  log: ['query', 'info', 'warn', 'error'],
+});
 
 // 新規会員登録
-    const signupUser=async(userData:any):Promise<User>=>{
-        try {
-        // パスワードハッシュ化
-        const saltRounds = 10;
-        const hassedPassword:string=await bcrypt.hash(userData.password,saltRounds)
-        const newUser:User=await prisma.user.create({
-            data:{
-                email:userData.email,
-                name:userData.name,
-                password:hassedPassword
-            },
-        })
-        await prisma.$disconnect();
-        return newUser;
-        } catch (error) {
-            await prisma.$disconnect();
-            throw new Error("そのメールアドレスはすでに登録されています");
-        }
-        
-    }
+const signupUser = async (userData: any): Promise<User> => {
+  try {
+    // パスワードハッシュ化
+    const saltRounds = 10;
+    const hassedPassword: string = await bcrypt.hash(userData.password, saltRounds)
+    const newUser: User = await prisma.user.create({
+      data: {
+        email: userData.email,
+        name: userData.name,
+        password: hassedPassword
+      },
+    })
+    await prisma.$disconnect();
+    return newUser;
+  } catch (error) {
+    await prisma.$disconnect();
+    throw new Error("そのメールアドレスはすでに登録されています");
+  }
+
+}
 export default {
-    signupUser,
+  signupUser,
 }
